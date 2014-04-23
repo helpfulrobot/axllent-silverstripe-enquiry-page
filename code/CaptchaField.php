aae8cc1f1f39d7abda841a04786791829dafcc89
Ralph Slooten <ralph@technojoy.co.nz>
2014-04-24 09:50:18 +1200

    Controller for captcha - fixes issue #6

--- code/CaptchaField.php
+++ code/CaptchaField.php
@@ -2,11 +2,11 @@
 
 class CaptchaField extends TextField {
 
-	function __construct($name, $title = null, $value = "", $form = null){
+	public function __construct($name, $title = null, $value = "", $form = null){
 		parent::__construct($name, $title, $value, $form);
 	}
 
-	function Field($properties = array()) {
+	public function Field($properties = array()) {
 		$attributes = array(
 			'type' => 'text',
 			'class' => 'CaptchaField',
@@ -19,7 +19,7 @@ class CaptchaField extends TextField {
 		);
 
 		// create link to image to display code
-		$html =  '<img src="' . _EnquiryPageBase .'/images/captcha.php?' . time() . '" class="customcaptcha-image" alt="CAPTCHA security code" width="60" height="24" />';
+		$html =  '<img src="' . $this->getForm()->getController()->Link() .'captcha.jpg?' . time() . '" class="customcaptcha-image" alt="CAPTCHA security code" width="60" height="24" />';
 
 		// create input field
 		$html .= $this->createTag('input', $attributes);
@@ -31,7 +31,7 @@ class CaptchaField extends TextField {
 
 	// SERVER-SIDE VALIDATION (to ensure a browser with javascript disabled doesn't bypass validation)
 
-	function validate($validator){
+	public function validate($validator){
 
 		$this->value = trim($this->value);
 
--- code/EnquiryPage.php
+++ code/EnquiryPage.php
@@ -11,9 +11,9 @@
 
 class EnquiryPage extends Page {
 
-	static $icon = 'silverstripe-enquiry-page/images/EnquiryPage.png';
+	public static $icon = 'silverstripe-enquiry-page/images/EnquiryPage.png';
 
-	static $description = 'Page with an editable contact form';
+	public static $description = 'Page with an editable contact form';
 
 	public static $db = array(
 		'EmailTo' => 'Varchar(254)',
@@ -163,9 +163,7 @@ class EnquiryPage extends Page {
 
 class EnquiryPage_Controller extends Page_Controller {
 
-	static $allowed_actions = array(
-		'EnquiryForm'
-	);
+	public static $allowed_actions = array('EnquiryForm', 'captcha');
 
 	public function init() {
 		parent::init();
@@ -293,7 +291,7 @@ class EnquiryPage_Controller extends Page_Controller {
 	}
 
 
-	function SendEnquiryForm($data, $form) {
+	public function SendEnquiryForm($data, $form) {
 		$From = $this->EmailFrom;
 		$To = $this->EmailTo;
 		$Subject = $this->EmailSubject;
@@ -322,6 +320,40 @@ class EnquiryPage_Controller extends Page_Controller {
 
 	public function Success() {
 		return isset($_GET['success']);
+	}
+
+	/*
+	 * Captcha image
+	 */
+	public function captcha() {
+		$this->response = new SS_HTTPResponse();
+		$this->response->addHeader('Content-Type', 'image/jpeg');
+		$width = 60;
+		$height = 24;
+		$my_image = imagecreatetruecolor($width, $height);
+		imagefill($my_image, 0, 0, 0xFFFFFF);
+		$purple = imageColorAllocate($my_image, 200, 0, 255);
+		$black = imageColorAllocate($my_image, 255, 255, 255);
+		$green = imageColorAllocate($my_image, 22, 255, 2);
+		$random_colours = array($purple,$green,$black);
+		// add noise
+		for ($c = 0; $c < 150; $c++){
+			$x = rand(0,$width-1);
+			$y = rand(0,$height-1);
+			imagesetpixel($my_image, $x, $y, $random_colours[array_rand($random_colours)]);
+		}
+		$x = rand(1,10);
+		$rand_string = rand(1000,9999);
+		$numbers = str_split($rand_string);
+		foreach ($numbers as $number){
+			$y = rand(1,10);
+			imagestring($my_image, 5, $x, $y, $number, 0x000000);
+			$x = $x+12;
+		}
+		Session::set('customcaptcha', md5($rand_string.$_SERVER['REMOTE_ADDR']).'a4xn');
+		$this->response->setBody(imagejpeg($my_image));
+		imagedestroy($my_image);
+		return $this->response;
 	}
 
 }
